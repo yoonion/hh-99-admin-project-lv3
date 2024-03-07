f7af77869eb04de40709ded42c86b0efe279daae
yoonion <yoonion97@gmail.com>
2024-03-07 14:54:21 +0900

    feat: GlobalExceptionHandler -   AccessDeniedException 예외 추가. 권한 없을 경우 거부 메시지 응답

--- src/main/java/com/sparta/admin/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/admin/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sparta.admin.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j(topic = "GlobalExceptionHandler - 예외 처리")
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Spring Security - 권한 없을 때 예외 (@Secured)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+        log.info("AccessDeniedException 진입");
+
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(IllegalArgumentException ex) {
